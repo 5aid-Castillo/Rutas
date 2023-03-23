122aa677bcba04af1a123a761794a14a66a61e5b
Said Castillo <said557@outlook.es>
2023-03-23 00:02:05 -0600

    gh-pages added

--- src/data/CrearListado.jsx
+++ src/data/CrearListado.jsx
@@ -1,15 +1,146 @@
 import React from 'react'
 import Destino from '../pages/Destino';
-import data from './data.js'
+/* import data from './data.js' */
 import '../styles/Destinos.css';
 import {motion} from 'framer-motion';
 
+import img1 from '../assets/cafayate.jpg'
+import img2 from '../assets/pncardones.jpg';
+import img3 from '../assets/polvorilla.jpg';
+import img4 from '../assets/iruya.jpg';
+import img5 from '../assets/buenosaires.jpg';
+import img6 from '../assets/cataratas.jpg';
+import img7 from '../assets/rosario.jpg';
+import img8 from '../assets/cordoba.jpg';
+import img9 from '../assets/mendoza.jpg';
+import img10 from '../assets/cumbrecita.jpg';
+import img11 from '../assets/chiquitalake.jpg';
+import img12 from '../assets/madryn.jpg';
+import img13 from '../assets/parque.jpg';
+import img14 from '../assets/rada.jpg';
+
+const Data = [
+    {
+       nombre:'Cafayate',
+       zona:'no',
+       imagen:img1,
+       situacion:'Localidad de los valles Calchaquíes de la provincia de Salta.',
+       servicio:'Visita a las formaciones de roca caliza y viñedos',
+       precio:90
+    },
+    {
+       nombre:'Parque Nacional Los Cardones',
+       zona:'no',
+       imagen:img2,
+       situacion:'Cerca de la localidad de Payogasta (Selva de las Yungas).',
+       servicio:'Caminata por la desértica belleza del Parque Nacional',
+       precio:85
+    },
+    {
+       nombre:'Viaducto La Polvorilla',
+       zona:'no',
+       imagen:img3,
+       situacion:'Recorrido en el Tren a las Nubes (desde Salta).',
+       servicio:"Recorrido por los viaductos del Tren a las Nubes del ramal C-14",
+       precio:124
+    },
+    {
+       nombre:'Iruya',
+       zona:'no',
+       imagen:img4,
+       situacion:'Provincia de Salta (Sierra de Santa Victoria).',
+       servicio:"Recorrido por la zona (Mirador, Río, iglesias..)",
+       precio:78
+    },
+    {
+       nombre:'Buenos Aires',
+       zona:'no',
+       imagen:img5,
+       situacion:'Ciudad autónoma y capital federal de Argentina.',
+       servicio:"Visita los barrios y plazas más representativos",
+       precio:195
+    },
+    {
+       nombre:'Cataratas del Iguazú',
+       zona:'no',
+       imagen:img6,
+       situacion:'Entre las provincias de Misiones y Paraná.',
+       servicio:"Recorrido por los diferentes senderos (Macuco..)",
+       precio:172
+    },
+   
+    {
+       nombre:'Rosario',
+       zona:'no',
+       imagen:img7,
+       situacion:'Sureste de la Provincia de Santa Fé del Departamento de Rosario.',
+       servicio:"Recorrido por el Monumento, museos y parques",
+       precio:79
+    },
+    {
+       nombre:'Córdoba',
+       zona:'cp',
+       imagen:img8,
+       situacion:'Capital de la Provincia de Córdoba (a orillas del río Suquía).',
+       servicio:"Visita a Altas Cumbres, Capuchinos, 100 curvas y Museos",
+       precio:85
+    },
+    {
+       nombre:'Mendoza',
+       zona:'cp',
+       imagen:img9,
+       situacion:'Capital de la Provincia, al este de la Cordillera de los Andes.',
+       servicio:"Visita al Aconcagua, viñedos, bodegas y parques",
+       precio:71
+    },
+    {
+       nombre:'La cumbrecita',
+       zona:'cp',
+       imagen:img10,
+       situacion:'Valle de Calamuchita (Sierras Grandes, Provincia de Córdoba).',
+       servicio:"Visita a las cascadas, peñón y río subterráneo",
+       precio:83
+    },
+    {
+       nombre:'Mar Chiquita Lake',
+       zona:'cp',
+       imagen:img11,
+       situacion:'Noroeste de la Provincia de Córdoba (Santiago del Estero).',
+       servicio:"Paseo en bote por la costonera, playa y el Mistolar",
+       precio:182
+    },
+    {
+       nombre:'Puerto Madryn',
+       zona:'cp',
+       imagen:img12,
+       situacion:'Noroeste de la Provincia de Chubut (Dpto. de Biedma).',
+       servicio:"Ver las ballenas y lobos marinos en Punta Loma",
+       precio:197
+    },
+    {
+       nombre:'Parque Nal. Monte León',
+       zona:'cp',
+       imagen:img13,
+       situacion:'Litoral marítimo de la Provincia de Santa Cruz (Patagonia).',
+       servicio:"Recorrido por los senderos y lagos del Parque",
+       precio:69
+    },
+    {
+       nombre:'Rada Tilly',
+       zona:'cp',
+       imagen:img14,
+       situacion:'Provincia del Chubut (a 15 km. al sur de Comodoro Rivadavia).',
+       servicio:"Visita a la Reserva Natural y Museo Regional",
+       precio:112
+    },
+
+ ]
 
 
 const CrearListado = ({zona}) => {
 
     /* para que solo anada aquellos elementos que coincidan */
-    const listado = data.filter(destino => 
+    const listado = Data.filter(destino => 
             destino.zona === zona)
             console.log(listado)
     return (
